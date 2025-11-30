7ca7b54273c6e49d9fa9cd6611589520cb93dcd2
na3Er <na3er.faraji@gmail.com>
2025-11-30 20:51:35 +0330

    feat: add create and update date to user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,12 @@
 import { Exclude } from 'class-transformer';
 import { UserRole } from 'src/common/enums/user-role.enum';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -16,4 +22,10 @@ export class User {
 
   @Column({ default: 'user' })
   role: UserRole;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
